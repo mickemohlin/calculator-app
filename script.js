0961353db47558c59f0b83ad17fc957f4befae3a
mickemohlin <mickemohlin@live.se>
2021-12-27 00:52:51 +0100

    Add output functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello World!");
+function calculate(leftOperand, operator, rightOperand) {
+    console.log("equals!!");
+}
+
+
+function buttonPressed(icon) {
+    if(icon != undefined || icon != null){
+        switch(icon){
+            case "C":
+                document.getElementById("output").innerText = "0";
+                break;
+            case "=":
+                calculate(1, "+", 1);
+                break;
+            default:
+                updateOutputField(icon);
+            
+        }
+    }
+}
+
+function updateOutputField(icon) {
+
+    let existingOutput = document.getElementById("output").innerText
+
+    if(existingOutput == "0") {
+        document.getElementById("output").innerText = icon;
+    } else {
+        existingOutput += icon;
+        document.getElementById("output").innerText = existingOutput;
+    }
+}
