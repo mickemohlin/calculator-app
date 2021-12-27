915d717f702cdc6c10ed7ebe7768a5f36d999e31
mickemohlin <mickemohlin@live.se>
2021-12-27 02:05:18 +0100

    Added logic to calculator

--- script.js
+++ script.js
@@ -1,26 +1,91 @@
-function calculate(leftOperand, operator, rightOperand) {
-    console.log("equals!!");
-}
+const ERROR_MESSAGE = "ERROR";
 
+var leftOperand;
+var rightOperand;
+var operator;
+var numberOfOperators = 0;
 
 function buttonPressed(icon) {
     if(icon != undefined || icon != null){
         switch(icon){
             case "C":
-                document.getElementById("output").innerText = "0";
+                reset();
+                updateOutput("0");
                 break;
             case "=":
-                calculate(1, "+", 1);
+                calculate();
                 break;
             default:
-                updateOutputField(icon);
-            
+                updateLogic(icon);
+                updateInput(icon);
         }
     }
 }
 
-function updateOutputField(icon) {
 
+function updateLogic(icon) {
+    if(operator == undefined) {
+        if(isNaN(icon)) {
+            operator = icon;
+            return;
+        }
+
+        // Update left operand:
+        if(leftOperand == undefined) { 
+            leftOperand = icon;
+        } else {
+            leftOperand += icon;
+        }
+    } else { 
+        // Update right operand:
+        if(rightOperand == undefined) {
+            rightOperand = icon;
+        } else {
+            rightOperand += icon;
+        }
+    }
+}
+
+
+function calculate() {
+    if(leftOperand == undefined || operator == undefined || rightOperand == undefined) {
+        updateOutput(ERROR_MESSAGE);
+        return;
+    } else {
+        let leftOperandInt = parseInt(leftOperand);
+        let rightOperandInt = parseInt(rightOperand);
+
+        let result;
+
+        switch(operator){
+            case "+":
+                result = leftOperandInt + rightOperandInt;
+                break;
+            case "-":
+                result = leftOperandInt - rightOperandInt;
+                break;
+            case "x":
+                result = leftOperandInt * rightOperandInt;
+                break;
+            case "/":
+                if(rightOperandInt == 0){
+                    result = ERROR_MESSAGE
+                } else {
+                    result = leftOperandInt / rightOperandInt;
+                }
+                break;
+            default:
+                updateOutput(ERROR_MESSAGE);
+                reset();
+                return;
+        }
+
+        reset();
+        updateOutput(result);
+    }
+}
+
+function updateInput(icon) {
     let existingOutput = document.getElementById("output").innerText
 
     if(existingOutput == "0") {
@@ -29,4 +94,16 @@ function updateOutputField(icon) {
         existingOutput += icon;
         document.getElementById("output").innerText = existingOutput;
     }
+}
+
+
+function reset() {
+    leftOperand = undefined;
+    rightOperand = undefined;
+    operator = undefined;
+}
+
+
+function updateOutput(result) {
+    document.getElementById("output").innerText = result;
 }
